8031809b25067c798389fd3ba61a1385e188ce05
Fangge Deng <lessdfg@outlook.com>
2017-03-26 00:53:06 -0400

    Fix the link between pages

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ db = SQLAlchemy(app, session_options={'autocommit': False})
 @app.route('/')
 def all_memes():
     memes = db.session.query(models.Meme).all()
-    return render_template('match-results-pg.html',memes=memes)
+    return render_template('meme-pg.html',memes=memes)
 
 '''
 @app.route('/drinker/<name>')
