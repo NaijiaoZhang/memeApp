0d4d2ccef98710c6b2eb1666070109ac46c85cc6
Naijiao <nz21@duke.edu>
2017-04-15 16:32:28 -0400

    local changes added

--- app.py
+++ app.py
@@ -9,15 +9,6 @@ app.secret_key = 's3cr3t'
 app.config.from_object('config')
 db = SQLAlchemy(app, session_options={'autocommit': False})
 
-'''
-@app.route('/')
-def landing_page():
-<<<<<<< HEAD
-    return render_template('layout.html')
-=======
-    memes = db.session.query(models.Meme).all()
-    return render_template('meme-pg-new.html',memes=memes)'''
->>>>>>> 250226ec97a37054e0d6214e090c8a3c4d1e1ce7
 
 @app.route('/discover')
 def discover_page():
@@ -37,8 +28,8 @@ def login():
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
+            return redirect(url_for('profile'))
+    return render_template('layout.html', error=error)
 
 @app.route('/profile')
 def profile_page():     
@@ -50,7 +41,7 @@ def profile_page():
 def landing_page():
     
     meme = db.session.query(models.Meme).filter(models.Meme.memeid == 4).one()
-    
+
     if request.method == 'POST':
         #if request.form['submit'] == 'NO':
         opinion = models.Opinion(2, 4, 1)
@@ -68,45 +59,8 @@ def landing_page():
             db.session.commit()
             flash('+Record was successfully added')
             meme = db.session.query(models.Meme).filter(models.Meme.memeid == 2).one()'''
-           
-    
     return render_template('meme-pg-new.html', meme = meme)
 
-'''
-@app.route('/drinker/<name>')
-def drinker(name):
-    drinker = db.session.query(models.Drinker)\
-        .filter(models.Drinker.name == name).one()
-    return render_template('drinker.html', drinker=drinker)
-
-@app.route('/edit-drinker/<name>', methods=['GET', 'POST'])
-def edit_drinker(name):
-    drinker = db.session.query(models.Drinker)\
-        .filter(models.Drinker.name == name).one()
-    beers = db.session.query(models.Beer).all()
-    bars = db.session.query(models.Bar).all()
-    form = forms.DrinkerEditFormFactory.form(drinker, beers, bars)
-    if form.validate_on_submit():
-        try:
-            form.errors.pop('database', None)
-            models.Drinker.edit(name, form.name.data, form.address.data,
-                                form.get_beers_liked(), form.get_bars_frequented())
-            return redirect(url_for('drinker', name=form.name.data))
-        except BaseException as e:
-            form.errors['database'] = str(e)
-            return render_template('edit-drinker.html', drinker=drinker, form=form)
-    else:
-        return render_template('edit-drinker.html', drinker=drinker, form=form)
-
-@app.template_filter('pluralize')
-def pluralize(number, singular='', plural='s'):
-    return singular if number in (0, 1) else plural
-'''
 if __name__ == '__main__':
-<<<<<<< HEAD
-=======
-
-    print "HIHIHI"
->>>>>>> 250226ec97a37054e0d6214e090c8a3c4d1e1ce7
     port = int(os.environ.get("PORT",5000))
     app.run(host='0.0.0.0', port=port)
--- meme.sql
+++ meme.sql
@@ -128,9 +128,6 @@ INSERT INTO PotentialPartner VALUES(2, 3);
 INSERT INTO PotentialPartner VALUES(2, 4);
 INSERT INTO PotentialPartner VALUES(2, 6);
 
-
-
-
 INSERT INTO Opinion VALUES(2, 1, 1);
 INSERT INTO Opinion VALUES(2, 2, 3);
 INSERT INTO Opinion VALUES(2, 3, 3);
