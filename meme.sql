9d730624aa10ba105112be221f6c78aefc6ac2fd
Naijiao <naijiaozhang489@gmail.com>
2017-04-23 03:32:16 -0400

    resetting

--- meme.sql
+++ meme.sql
@@ -40,7 +40,6 @@ CREATE TABLE Opinion
 uid INTEGER NOT NULL REFERENCES Users(uid),
 memeID INTEGER NOT NULL REFERENCES Meme(memeID),
 preference INTEGER NOT NULL,
-PRIMARY KEY(uid, memeID)
 -- #0=neutral, 1=likes, -1=dislike
 );
 
